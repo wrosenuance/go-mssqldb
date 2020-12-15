834187b23ea69148252ec6f058cea5df0e84a26c
Rose, William <william.rose@nuance.com>
2020-12-15 14:24:21 -0800

    Revise fed auth cert expectation to PEM instead of deprecated PKCS#12

--- fedauth.go
+++ fedauth.go
@@ -3,11 +3,10 @@ package mssql
 import (
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"io/ioutil"
-
-	"golang.org/x/crypto/pkcs12"
 )
 
 // FederatedAuthenticationConfigurer implementations use the connection string
@@ -47,7 +46,7 @@ func (c *Connector) checkFedAuthProviders(params *connectParams) error {
 
 	// Ignore DSNs that don't request one of the supported federated authentication
 	// libraries.
-	if params.fedAuthLibrary != fedAuthLibrarySecurityToken && params.fedAuthLibrary != fedAuthLibraryADAL {
+	if params.fedAuthLibrary == fedAuthLibraryReserved {
 		return nil
 	}
 
@@ -83,21 +82,52 @@ func (c *Connector) checkFedAuthProviders(params *connectParams) error {
 	return nil
 }
 
-func getFedAuthClientCertificate(clientCertPath, clientCertPassword string) (*x509.Certificate, *rsa.PrivateKey, error) {
-	pkcs, err := ioutil.ReadFile(clientCertPath)
+func getFedAuthClientCertificate(clientCertPath, clientCertPassword string) (certificate *x509.Certificate, privateKey *rsa.PrivateKey, err error) {
+	pemBytes, err := ioutil.ReadFile(clientCertPath)
 	if err != nil {
-		return nil, nil, fmt.Errorf("Failed to read the AD client certificate from path %s: %v", clientCertPath, err)
 	}
 
-	privateKey, certificate, err := pkcs12.Decode(pkcs, clientCertPassword)
+	var block, encryptedPrivateKey *pem.Block
+	var certificateBytes, privateKeyBytes []byte
+
+	for block, pemBytes = pem.Decode(pemBytes); block != nil; block, pemBytes = pem.Decode(pemBytes) {
+		_, dekInfo := block.Headers["DEK-Info"]
+		switch {
+		case block.Type == "CERTIFICATE":
+			certificateBytes = block.Bytes
+		case block.Type == "RSA PRIVATE KEY" && dekInfo:
+			encryptedPrivateKey = block
+		case block.Type == "RSA PRIVATE KEY":
+			privateKeyBytes = block.Bytes
+		default:
+			return nil, nil, fmt.Errorf("PEM file %s contains unsupported block type %s", clientCertPath, block.Type)
+		}
+	}
+
+	if len(certificateBytes) == 0 {
+		return nil, nil, fmt.Errorf("No certificate found in PEM file at path %s: %v", clientCertPath, err)
+	}
+
+	certificate, err = x509.ParseCertificate(certificateBytes)
 	if err != nil {
-		return nil, nil, fmt.Errorf("Failed to read the AD client certificate from path %s: %v", clientCertPath, err)
+		return nil, nil, fmt.Errorf("Failed to parse certificate found in PEM file at path %s: %v", clientCertPath, err)
 	}
 
-	rsaPrivateKey, isRsaKey := privateKey.(*rsa.PrivateKey)
-	if !isRsaKey {
-		return nil, nil, fmt.Errorf("AD client certificate at path %s must contain an RSA private key", clientCertPath)
+	if encryptedPrivateKey != nil {
+		privateKeyBytes, err = x509.DecryptPEMBlock(encryptedPrivateKey, []byte(clientCertPassword))
+		if err != nil {
+			return nil, nil, fmt.Errorf("Failed to decrypt private key found in PEM file at path %s: %v", clientCertPath, err)
+		}
 	}
 
-	return certificate, rsaPrivateKey, nil
+	if len(privateKeyBytes) == 0 {
+		return nil, nil, fmt.Errorf("No private key found in PEM file at path %s: %v", clientCertPath, err)
+	}
+
+	privateKey, err = x509.ParsePKCS1PrivateKey(privateKeyBytes)
+	if err != nil {
+		return nil, nil, fmt.Errorf("Failed to parse private key found in PEM file at path %s: %v", clientCertPath, err)
+	}
+
+	return
 }
